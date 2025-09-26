f8504b756ec2d2bbce98d9358b05e62040a7adc3
Korakbhakta02 <67550363+Korakbhakta02@users.noreply.github.com>
2025-09-26 09:43:25 +0530

    Update openai.js

--- src/openai.js
+++ src/openai.js
@@ -1,6 +1,6 @@
 const { OpenAI }=require('openai')
 const openai= new OpenAI({
-    apiKey: "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", dangerouslyAllowBrowser: true
+    apiKey: "", dangerouslyAllowBrowser: true
 })
 
 export async function sendmsgtoopenai(message){
